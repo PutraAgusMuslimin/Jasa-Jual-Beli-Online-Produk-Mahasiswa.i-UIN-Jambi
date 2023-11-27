c7aca7bedac76d86070fb2ea0c7dfd182bf926af
MSPUTERRR <134085738+putrajogo93@users.noreply.github.com>
2023-11-27 16:10:48 +0700

    tokouinku

---
+++ dbcon.php
@@ -0,0 +1,6 @@
+<?php
+$host = "localhost";
+$tableprefix = "db_touin_"; //You can change this table prefix, don't use white spaces, use underscores instead for example this_is_my_table_prefix_
+$databasename = "db_touin";
+$dbuser = "root";
+$dbpassword = "";
